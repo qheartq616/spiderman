937f389b440e7a6c0d21c5b98f6782ae72c91d02
Y02483 <1359582196@qq.com>
2023-04-13 18:24:51 +0800

    使用htmlunit

--- src/main/java/com/qheartq616/spiderman/controller/SiteController.java
+++ src/main/java/com/qheartq616/spiderman/controller/SiteController.java
@@ -1,5 +1,8 @@
 package com.qheartq616.spiderman.controller;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.qheartq616.spiderman.config.SiteConfig;
 import com.qheartq616.spiderman.constant.Constant;
 import com.qheartq616.spiderman.pojo.Site;
@@ -14,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,39 +34,59 @@ public class SiteController {
 
         for (Site site : siteList) {
             try {
-                Document document = Jsoup.connect(site.getUrl())
-                        .header("User-Agent", Constant.userAgent)
-                        .get();
+//                Document document = Jsoup.connect(site.getUrl())
+//                        .header("User-Agent", Constant.userAgent)
+//                        .get();
+
+                //Htmlunit模拟的浏览器，设置css,js等支持及其它的一些简单设置
+                WebClient browser = new WebClient();
+                browser.getOptions().setCssEnabled(false);
+                browser.getOptions().setJavaScriptEnabled(true);
+                browser.getOptions().setThrowExceptionOnScriptError(false);
+                //获取页面
+                HtmlPage htmlPage = browser.getPage(site.getUrl());
+                //设置等待js的加载时间
+                browser.waitForBackgroundJavaScript(3000);
+                //使用xml的方式解析获取到jsoup的document对象
+                Document document = Jsoup.parse(htmlPage.asXml());
 
                 Elements elements = document.select(site.getSelect());
-                //遍历批文
+                //遍历批文元素
                 for (Element element : elements) {
+                    //只能遍历a标签的链接  onclick事件无法爬取
                     String href = element.attr("href");
                     String title = element.ownText();
+                    //地址是否完整，不完整需要分相对路径和绝对路径两种补全地址
                     if (!href.contains("http")){
                         href = site.getBaseUrl()+href;
+                        //todo
                     }
 
                     Document shenPiHref = Jsoup.connect(href).get();
                     Elements shenPiElements = shenPiHref.select("*");
-
+                    //遍历a标签链接网页所有元素
                     for (Element shenPiElement : shenPiElements) {
                         String text = shenPiElement.ownText();
                         if (text.contains(key)){
+                            //成功搜到
                             returnMap.put(site.getName(),"成功找到：标题-"+title+"-链接-"+href);
                             break;
                         }
                     }
                 }
-
+            //失败
             } catch (HttpStatusException e){
                 e.printStackTrace();
-                returnMap.put(site.getName(),"失败！状态码为："+e.getStatusCode()+"。具体原因为："+e.toString());
+                returnMap.put(site.getName(),"失败！状态码为："+e.getStatusCode()+"。具体原因为："+e);
+            } catch (FailingHttpStatusCodeException e){
+                e.printStackTrace();
+                returnMap.put(site.getName(),"失败！状态码为："+e.getStatusCode()+"。具体原因为："+e);
             } catch (IOException e) {
                 e.printStackTrace();
-                throw new RuntimeException(e);
+                returnMap.put(site.getName(),"失败！具体原因为："+e);
             } finally {
                 if (returnMap.get(site.getName())==null){
+                    //无结果
                     returnMap.put(site.getName(),"无搜索结果");
                 }
             }
